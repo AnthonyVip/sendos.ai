c33c5da01790826f305fbaaa6bd2921264d7e5fc
Anthony Diaz <anthony.gdmou@gmail.com>
2024-06-18 13:44:42 -0500

    Inicializar el proyecto con FastAPI
    feat: Inicializa el proyecto con python y fastapi
    
    - Configuracion basica del entorno de desarrollo.
    - Estructura de carpetas y archivos iniciales para el servicio.

---
+++ core/settings.py
@@ -0,0 +1,78 @@
+import os
+from enum import Enum
+from functools import lru_cache
+from typing import List, Optional, Tuple
+
+from dotenv import load_dotenv
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class EnvironmentEnum(str, Enum):
+    production = "production"
+    local = "local"
+    devel = "devel"
+
+
+class GlobalConfig(BaseSettings):
+    title: str = "Celes API"
+    docs_url: str = "/docs"
+    description: str = "This is the Celes API"
+    version: str = "1.0.0"
+    openapi_url: str = "/openapi.json"
+    redoc_url: str = "/redoc"
+    api_prefix: str = "/api"
+    openapi_prefix: str = os.environ.get("OPENAPI_PREFIX")
+    secret_key: SecretStr = os.environ.get("SECRET_KEY")
+    allowed_hosts: List[str] = ["*"]
+    loggers: Tuple[str, str] = ("uvicorn.asgi", "uvicorn.access")
+    environment: EnvironmentEnum
+    debug: bool = False
+    timezone: str = "UTC"
+    jwt_subject: str = os.environ.get("JWT_SUBJECT")
+    algorithm: str = os.environ.get("ALGORITHM")
+    access_token_expire_minutes: int = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")  # noqa: E501
+
+    class Config:
+        case_sensitive = True
+
+
+class LocalConfig(GlobalConfig):
+    debug: bool = True
+    environment: EnvironmentEnum = EnvironmentEnum.local
+
+
+class DevelConfig(GlobalConfig):
+    debug: bool = True
+    environment: EnvironmentEnum = EnvironmentEnum.devel
+
+
+class ProdConfig(GlobalConfig):
+    debug: bool = False
+    environment: EnvironmentEnum = EnvironmentEnum.production
+
+
+class FactoryConfig:
+    def __init__(self, environment: Optional[str]):
+        self.environment = environment
+        self.configurations_maps = {
+            EnvironmentEnum.local.value: LocalConfig,
+            EnvironmentEnum.devel.value: DevelConfig,
+            EnvironmentEnum.production.value: ProdConfig,
+        }
+
+    def __call__(self) -> GlobalConfig:
+        return self.configurations_maps[self.environment]()
+
+
+@lru_cache()
+def get_configuration() -> GlobalConfig:
+    try:
+        return FactoryConfig(os.environ.get("ENVIRONMENT"))()
+    except KeyError:
+        return FactoryConfig("local")()
+
+
+settings = get_configuration()
---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
+from starlette.middleware.cors import CORSMiddleware
+
+from routes.errors.http_error import http_error_handler
+from routes.errors.validation_error import http422_error_handler
+from core.settings import settings
+
+
+def get_application() -> FastAPI:
+    application = FastAPI(
+        title=settings.title,
+        description=settings.description,
+        version=settings.version,
+        debug=settings.debug,
+        docs_url=settings.docs_url,
+        redoc_url=settings.redoc_url,
+        openapi_url=settings.openapi_url,
+        openapi_prefix=settings.openapi_prefix,
+    )
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.allowed_hosts,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    application.add_exception_handler(
+        HTTPException,
+        http_error_handler
+    )
+
+    application.add_exception_handler(
+        RequestValidationError,
+        http422_error_handler
+    )
+
+    return application
+
+
+app = get_application()
+
+
+@app.get("/")
+def home():
+    return {"Service": "Sendos.AI!"}
